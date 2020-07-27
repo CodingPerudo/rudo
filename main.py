954d8f37c8fcf572b661bc24e63e5eea0082af86
Michelle Loven <ml2359@cornell.edu>
2020-07-27 17:34:48 -0400

    Adding comments

--- main.py
+++ main.py
@@ -2,12 +2,48 @@ import json
 from flask import Flask, request
 app = Flask(__name__)
 
+#make a server code to login to the game (have multiple sessions)
+#entering a name/ username per cup
+ 
+
+#USING DIFF SERVER PYTHON STUFF
+
+
+#cheeky round!
+    #disable increasing face num
+#we have to get rid of dice
+    #losing -> gray 
+
+
+
+#militsa 
+    #dudo button
+        #says who clicked it 
+    #exacto button
+        #only available to a few people 
+    #paste in example get and example post
+
+#michelle
+    #display dice and reroll dice 
+
+#peter
+    #make cups go in order 
+        #all cups are blank 
+        #assign color
+    #probability counter
+    #obvious whos turn it is 
+        #opacity background highlights current player
+    #we need a way to make a bet
+        #input boxes with only increasing steps unless converting to ones (With a conversion button)
+
 
 #___________________________________Information on all players_________________________________________
 playerInfo= {
     "red": {
         "dice_nums": [0,0,0,0,0],
         "displayed_dice": [0,0,0,0,0]
+        # store next player
+        #maybe make the displayed/dice nums = -1 when they lose a die
     },
     "orange": {
         "dice_nums": [0,0,0,0,0],
@@ -20,15 +56,17 @@ playerInfo= {
     "green": {
         "dice_nums": [0,0,0,0,0],
         "displayed_dice": [0,0,0,0,0]
-    }
+    },
     "blue": {
         "dice_nums": [0,0,0,0,0],
         "displayed_dice": [0,0,0,0,0]
-    }
+    },
     "black": {
         "dice_nums": [0,0,0,0,0],
         "displayed_dice": [0,0,0,0,0]
     }
+
+    #current bet
 
 }
 
